a0ddd9907e1987f74c5dd4b07d06f610c807fbc3
k.sergey <higlyman@gmail.com>
2021-09-26 16:13:30 +0300

    some ref

--- books/src/main/java/ru/adespina/models/BooksStorage.java
+++ books/src/main/java/ru/adespina/models/BooksStorage.java
@@ -1,18 +1,32 @@
 package ru.adespina.models;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class BooksStorage {
     private static final Set<Book> books = new HashSet<>();
 
-    static {
-        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"java", "ad", 3));
-        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"js", "ps", 3));
-    }
-
     public static Set<Book> getBooks() {
         return books;
+    }
+
+    static {
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Star Wars: Lost Tribe of the Sith: The Collected Stories", "Джон Джексон Миллер", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Затерянное племя ситхов: На краю", "Джон Джексон Миллер", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Звёздные войны. Старая Республика: Реван", "Дрю Карпишин", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Звёздные войны. Старая Республика: Обманутые", "Пол С. Кемп", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Красная жатва", "Джо Шрайбер", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Звёздные войны. Старая Республика: Роковой союз", "Шон Уильямс", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Звёздные войны. Старая Республика: Истребление", "Дрю Карпишин", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Затерянное племя ситхов: Пантеон", "Джон Джексон Миллер", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Странствующий рыцарь", "Дрю Карпишин", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Наследие джедаев", "Джуд Уотсон", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Звёздные войны: Дарт Плэгас", "Джеймс Лучено", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Ученик джедая: Становление Силы", "Дейв Волвертон", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Ученик джедая: Властитель тёмной силы", "Джуд Уотсон", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Жизнь и легенда Оби-Вана Кеноби", "Райдер Виндхам", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Ученик джедая: Единственная свидетельница", "Джуд Уотсон", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Тайны джедаев", "Джуд Уотсон", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Восход и падение Дарта Вейдера", "Райдер Виндхам", new Random().nextInt(400)));
+        books.add(new Book(String.valueOf(new Random().nextInt(5000000)),"Дарт Мол: Диверсант", "Джеймс Лучено", new Random().nextInt(400)));
     }
 }

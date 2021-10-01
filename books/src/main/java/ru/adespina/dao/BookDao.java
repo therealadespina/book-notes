eed8fc1c016f941c6661052ea7262419ef28d70d
k.sergey <higlyman@gmail.com>
2021-10-01 21:17:47 +0300

    ref

--- books/src/main/java/ru/adespina/dao/BookDao.java
+++ books/src/main/java/ru/adespina/dao/BookDao.java
@@ -2,12 +2,14 @@ package ru.adespina.dao;
 
 import ru.adespina.models.Book;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface BookDao {
-    List<Book> findAll();
+    List<Book> findAll() throws SQLException;
     Book save(Book book);
     Book getById(Integer bookId);
     Book update(Book book);
     void delete(Book book);
+    void deleteAll();
 }
